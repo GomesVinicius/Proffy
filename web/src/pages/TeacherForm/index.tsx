5355884e0d44931f848afd18c706357c7ce3ca92
Vinícius Gomes <vinigc19@gmail.com>
2020-08-05 22:05:57 -0300

    Select Finished

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+import Textarea from '../../components/Textarea';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+import Select from '../../components/Select';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                title="Que incrível que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome completo" />
+
+                    <Input name="avatar" label="Avatar" />
+
+                    <Input type="whatsapp" name="WhatsApp" label="Horário" />
+
+                    <Textarea name="bio" label="Biografia" />
+
+                </fieldset>
+
+                <fieldset>
+                    
+                    <legend>Sobre a aula</legend>
+
+                    <Select 
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: "Português", label: "Português" },
+                            { value: "Matemática", label: "Matemática" },
+                            { value: "Geografia", label: "Geografia" },
+                        ]}
+                    />
+
+                    <Input name="cost" label="Custo da sua hora por aula" />
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                        Importante! <br />
+                        Preencha todos os campos
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
+
+        </div>
+    )
+}
+
+export default TeacherForm;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,30 +1,47 @@
 import React from 'react';
+import TeacherItem from '../../components/TeacherItem';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
+import Input from '../../components/Input';
 
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
 
-function TeacherList () {
+function TeacherList() {
     return (
         <header id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Horário</label>
-                        <input type="text" id="time" />
-                    </div>
+
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: "Português", label: "Português" },
+                            { value: "Matemática", label: "Matemática" },
+                            { value: "Geografia", label: "Geografia" },
+                        ]}
+                    />
+
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: "0", label: "Domingo" },
+                            { value: "1", label: "Segunda" },
+                            { value: "2", label: "Terça" },
+                            { value: "3", label: "Quarta" },
+                            { value: "4", label: "Quinta" },
+                            { value: "5", label: "Sexta" },
+                            { value: "6", label: "Sábado" },
+                        ]}
+                    />
+
+                    <Input type="time" name="time" label="Horário" />
+
                 </form>
-            
+
             </PageHeader>
-            
+
             <main>
                 <TeacherItem />
                 <TeacherItem />
